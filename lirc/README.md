d26b6c70862dd50137dfac333125373dbdaf4ec3
voje <kristjan.voje@gmail.com>
2017-09-22 14:27:18 +0200

    asdf

---
+++ lirc/README.md
@@ -0,0 +1,3 @@
+## Usage
+This is the copy of the last working configuration for lirc.  
+Usage: replace ```/etc/lirc``` with this ```./lirc``` folder.  
--- lirc/helper.sh
+++ lirc/helper.sh
@@ -3,4 +3,9 @@
 
 pipe="/home/pi/git/RPI_Projector/python_script/ir.fifo"
 
+#running:
 echo "$1" >"$pipe"
+/home/pi/git/RPI_Projector/logging/log_event.sh "lirc" "$1"
+
+#testing:
+#echo "$1" | tee -a "$pipe" "/home/pi/git/RPI_Projector/testing.log"
